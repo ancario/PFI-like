e95ca2b70412bd86ceec3145355a79a5725f43a2
ancario <anthonylabbe68@gmail.com>
2024-12-05 14:48:49 -0500

    account

---
+++ wwwroot/Posts/js/Accounts_API.js
@@ -0,0 +1,81 @@
+
+class Accounts_API {
+    static API_URL() { return "http://localhost:5000/api/accounts" };
+    static initHttpState() {
+        this.currentHttpError = "";
+        this.currentStatus = 0;
+        this.error = false;
+    }
+    static setHttpErrorState(xhr) {
+        if (xhr.responseJSON)
+            this.currentHttpError = xhr.responseJSON.error_description;
+        else
+            this.currentHttpError = xhr.statusText == 'error' ? "Service introuvable" : xhr.statusText;
+        this.currentStatus = xhr.status;
+        this.error = true;
+    }
+    static async HEAD() {
+        Accounts_API.initHttpState();
+        return new Promise(resolve => {
+            $.ajax({
+                url: this.API_URL(),
+                type: 'HEAD',
+                contentType: 'text/plain',
+                complete: data => { resolve(data.getResponseHeader('ETag')); },
+                error: (xhr) => { Accounts_API.setHttpErrorState(xhr); resolve(null); }
+            });
+        });
+    }
+    static async Get(id = null) {
+        Accounts_API.initHttpState();
+        return new Promise(resolve => {
+            $.ajax({
+                url: this.API_URL() + (id != null ? "/" + id : ""),
+                complete: data => { resolve({ ETag: data.getResponseHeader('ETag'), data: data.responseJSON }); },
+                error: (xhr) => { Accounts_API.setHttpErrorState(xhr); resolve(null); }
+            });
+        });
+    }
+    static async GetQuery(queryString = "") {
+        Accounts_API.initHttpState();
+        return new Promise(resolve => {
+            $.ajax({
+                url: this.API_URL() + queryString,
+                complete: data => {
+                    resolve({ ETag: data.getResponseHeader('ETag'), data: data.responseJSON });
+                },
+                error: (xhr) => {
+                    Accounts_API.setHttpErrorState(xhr); resolve(null);
+                }
+            });
+        });
+    }
+    static async Save(data, create = true) {
+        Accounts_API.initHttpState();
+        return new Promise(resolve => {
+            $.ajax({
+                url: create ? this.API_URL() : this.API_URL() + "/" + data.Id,
+                type: create ? "POST" : "PUT",
+                contentType: 'application/json',
+                data: JSON.stringify(data),
+                success: (data) => { resolve(data); },
+                error: (xhr) => { Accounts_API.setHttpErrorState(xhr); resolve(null); }
+            });
+        });
+    }
+    static async Delete(id) {
+        return new Promise(resolve => {
+            $.ajax({
+                url: this.API_URL() + "/" + id,
+                type: "DELETE",
+                complete: () => {
+                    Accounts_API.initHttpState();
+                    resolve(true);
+                },
+                error: (xhr) => {
+                    Accounts_API.setHttpErrorState(xhr); resolve(null);
+                }
+            });
+        });
+    }
+}
